595beb02b21be56ee4e78f811f27e8cd0fe86527
Sara Jakša <sarajaksa@gmail.com>
2018-11-22 13:10:41 +0100

    more minimal example

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
-// In App.js in a new project
-
-import React from "react";
-import { View, Text } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createStackNavigator} from 'react-navigation';
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
       </View>
     );
   }
 }
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
   }
-});
+}
+const Routes1 = createStackNavigator();
 
-export default createAppContainer(AppNavigator);
+export default Routes1;
