be54812a4a2026fa8ec9569bbb09109b2f556f17
Sara Jakša <sarajaksa@gmail.com>
2018-11-22 14:01:46 +0100

    Changing java to make react-navigation work

--- android/app/src/main/java/com/uexperience/MainActivity.java
+++ android/app/src/main/java/com/uexperience/MainActivity.java
@@ -2,6 +2,11 @@ package com.uexperience;
 
 import com.facebook.react.ReactActivity;
 
+/* For importing gestures for react navigation */
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -12,4 +17,17 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "uexperience";
     }
+
+  /* For importing gestures for react navigation */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+
+  
 }
