1d58fbaa4980e1060a89bf65f83d90d7818dff3e
Sara Jakša <sarajaksa@gmail.com>
2018-11-22 14:06:54 +0100

    Working example with each screen in each file

--- App.js
+++ App.js
@@ -1,33 +1,8 @@
-// In App.js in a new project
-
 import React from "react";
 import { View, Text, Button } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-          <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-          />
-      </View>
-    );
-  }
-}
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
-}
-
+import HomeScreen from "./src/HomeScreen.js";
+import DetailsScreen from "./src/DetailsScreen.js"
 
 const AppNavigator = createStackNavigator(
   {
