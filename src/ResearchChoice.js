ea73dce8e3873a292fddc6ec6eec5432c6da5143
Sara Jakša <sarajaksa@sarajaksa.eu>
2019-01-14 14:14:36 +0100

    API version 1
    This is not really an API yet. It is just a script, that takes all the
    research plans and puts them in one file, which can be fetched. This way
    I don't need to hard code the research plans, but I can only change one
    file on the internet.

--- src/ResearchChoice.js
+++ src/ResearchChoice.js
@@ -14,99 +14,39 @@ import {setDataToStorage, getDataFromStorage} from "./functions/data.js";
 import {newResearch} from "./functions/notifications.js";
 
 export default class ResearchChoice extends Component {
-    componentDidMount() {
+    componentDidMount = async () => {
         this.setState({
             password: this.props.navigation.getParam("password", ""),
         });
-    }
+        try {
+            var researchPlans = await fetch(
+                "https://testing.sarajaksa.eu/all_research_projects.json",
+            );
+        } catch (err) {
+            this.setState({
+                success:
+                    "Something went wrong. Is the internet connection working?",
+            });
+            return;
+        }
+        researchPlans = researchPlans._bodyInit;
+        try {
+            researchPlans = JSON.parse(researchPlans);
+        } catch {
+            this.setState({
+                success: "Something went wrong. Is you put in the right url?",
+            });
+            return;
+        }
+        this.setState({researchPlans: researchPlans});
+    };
 
     state = {
         success: "You did not try it yet",
         url: "",
         password: "",
         researchFromApi: true,
-        researchPlans: [
-            {
-                id: 1,
-                name: "Raziskovanje Spolnih Fantazij",
-                description:
-                    "Ta raziskava te prosi da raziskuješ svoje doživljanje spolnih fantazij",
-                organization: "Univerza v Ljubljani",
-                days: 10,
-                start: "2019-01-08",
-                end: "2019-05-31",
-                privacy: "",
-                researcher: "Katja",
-                email: "Katja@example.com",
-                subjectId: null,
-                share: false,
-                beeps: {
-                    aditionalCriteria: true,
-                    importantMomentsOnly: false,
-                    dailyBeeps: 20,
-                },
-                audio: false,
-                place: false,
-                picture: false,
-                descriptive: true,
-                questions: [
-                    {
-                        id: 1,
-                        question: "S kom si?",
-                        context: true,
-                        type: "Tags",
-                        current: 1,
-                    },
-                    {
-                        id: 2,
-                        question: "Kakšen je tvoj nivo vzburjenosti?",
-                        context: false,
-                        type: "Slider",
-                        current: 1,
-                    },
-                ],
-            },
-            {
-                id: 2,
-                name: "Raziskovanje Stresa",
-                description:
-                    "Ta raziskava raziskuje povezavo med stresom in različnimi dejavnostmi",
-                organization: "None",
-                days: 30,
-                start: "2019-01-08",
-                end: "2019-12-31",
-                privacy: "",
-                researcher: "Sara Jakša",
-                email: "sarajaksa@sarajaksa.eu",
-                subjectId: null,
-                share: false,
-                beeps: {
-                    aditionalCriteria: true,
-                    importantMomentsOnly: true,
-                    dailyBeeps: 3,
-                },
-                audio: false,
-                place: false,
-                picture: false,
-                descriptive: true,
-                questions: [
-                    {
-                        id: 1,
-                        question: "Kaj trenutno počneš?",
-                        context: true,
-                        type: "Tags",
-                        current: 1,
-                    },
-                    {
-                        id: 2,
-                        question: "Koliko stresa doživljaš?",
-                        context: false,
-                        type: "Slider",
-                        current: 1,
-                    },
-                ],
-            },
-        ],
+        researchPlans: [],
     };
 
     getQuestions = async url => {
